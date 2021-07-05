d38e65efab1f02125ae32849b50c1e891eaf1283
= <=>
2021-07-06 03:39:16 +0700

    fix bug

--- components/profile/Feed/index.js
+++ components/profile/Feed/index.js
@@ -8,54 +8,60 @@ import Loading from '../../commons/Loading';
 import { HOST, POSTS_API, PROFILE_API } from '../../../config/config';
 import { useRouter } from 'next/router';
 import { useSelector } from 'react-redux';
+import InfiniteScroll from 'react-infinite-scroller';
 
-function extractData(data, result = []) {
-    if (Array.isArray(data))
-        data.forEach(element => {
-            if (Array.isArray(element))
-                element.forEach(subele => {
-                    result.push(subele);
-                });
-            else result.push(element);
-        });
-    else result.push(data);
-    return result;
-}
 
 export default function Feed({user_id, permission}) {
     const token = useSelector(state => state.token)
     const [posts, setPosts] = useState([]);
+    const [hasMore, setHasMore] = useState(true);
+    const [nextUrl, setNextUrl] = useState(PROFILE_API + `post/${user_id}`);
 
-    useEffect(()=>{
-        axios.get(PROFILE_API + `post/${user_id}`, {
+    function loadMore(page){
+        let url = ''
+        if(nextUrl != null){
+            url = nextUrl;
+        }else{
+            return false;
+        }
+        axios.get(url, {
             headers: {
                 'Authorization': `Bearer ${token}`
-            }
+            },
+            cache: true
         }).then((response) => {
-            setPosts(extractData(response.data.data).sort(function (a, b) {
-                let time1 = new Date(a.updated_at);
-                let time2 = new Date(b.updated_at);
-    
-                return time2 - time1;
-            }));
+            let lists = posts;
+            lists = lists.concat(response.data.data.data);
+            setPosts(lists);
+            setNextUrl(response.data.data.next_page_url);
+            
+            if(response.data.data.next_page_url == null){
+                setHasMore(false);
+            }
         }).catch((error) => {
             console.log(error);
         })
-
-    },[null])
+    }
 
     return (
         <div className={styles.container}>
             {permission ? <div className={styles.row}>
                 <CreatePostForm list={posts} setList={setPosts} ></CreatePostForm>
             </div> : ''}
-            {posts.map((element, key) => {
-                return (
-                    <LazyLoad key={element.id} height={200} placeholder={<Loading />}>
-                        <Post key={element.id} post={element}></Post>
-                    </LazyLoad>
-                )
-            })}
+            <InfiniteScroll
+                pageStart={1}
+                loadMore={loadMore}
+                hasMore={hasMore}
+                loader={<div className="loader" key={0}>Loading ...</div>}
+            >
+                {posts.map((element, key) => {
+                    return (
+                        <LazyLoad key={element.id} height={200} placeholder={<Loading />}>
+                            <Post key={element.id} post={element}></Post>
+                        </LazyLoad>
+                    )
+                })}
+            </InfiniteScroll>
         </div>
     )
 }
