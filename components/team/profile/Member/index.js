c6fd8d7dac7055375d6f84a674c133054b24138d
= <=>
2021-07-05 20:46:12 +0700

    fix bug

--- components/team/profile/Member/index.js
+++ components/team/profile/Member/index.js
@@ -12,44 +12,25 @@ import styles from './styles.module.scss';
 export default function Member({team}){
     const [members, setMembers] = useState([]);
     const [membersC, setMemberC] = useState([]);
-    const [invite, setInvite] = useState([]);
     const token = useSelector(state => state.token);
 
     useEffect(()=>{
-        let isMounted = true;
         axios.get(TEAM_API+`${team.id}/member`,{
             headers:{
                 Authorization: `Bearer ${token}`
             }
         }).then(response=>{
             let data = response.data.data;
-            if(isMounted){
-                setMembers(data);
-                setMemberC(data);
-            }
+            setMembers(data);
+            setMemberC(data);
         }).catch(error => {
             console.log(error);
         });
-
-        axios.get(TEAM_API + `${team.id}/invite`, {
-            headers:{
-                Authorization: `Bearer ${token}`
-            }
-        }).then(response=>{
-            if(isMounted){
-                setInvite(response.data.data);
-            }
-        }).catch(error => {
-            if(isMounted)
-            console.log(error);
-        });
-
-        return () => { isMounted = false };
-    },[]);
+    },[null]);
 
     return(
         <div className={styles.container}>
-            <InviteForm friends={invite}></InviteForm>
+            <InviteForm team={team}></InviteForm>
             <div className={styles.list}>
             <Filter members={membersC} setMember={setMembers} result={members}></Filter>
             {members.map((element,key) => {
